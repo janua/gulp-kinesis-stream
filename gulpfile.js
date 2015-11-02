6206e7ac77e1b7bbf9a515834d0a078613ad847c
Francis Carr <fran@fcarr.com>
2015-11-02 13:46:59 +0000

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp   = require('gulp');
+var kinesis = require('kinesis');
+var KinesisStream = kinesis.KinesisStream;
+
+var STREAM_NAME = '';
+var OLDEST = false;
+var REGION = 'eu-west-1';
+
+var kinesisSource = kinesis.stream({region: REGION, name: STREAM_NAME, oldest: OLDEST});
+
+function extractDataFromKinesisEvent(event) {
+  return event.Data.toString("utf-8");
+};
+
+function dataPrinter(event) { 
+  console.log(extractDataFromKinesisEvent(event)); 
+};
+
+gulp.task('listen', function() {
+  return kinesisSource.on('data', dataPrinter);
+});
+
+gulp.task('listStreams', function() {
+  return kinesis.listStreams({region: REGION}, function(err, streams) {
+    if (err) throw err
+
+    console.log(streams)
+  });
+});
+
+gulp.task('default', ['listStreams']);
